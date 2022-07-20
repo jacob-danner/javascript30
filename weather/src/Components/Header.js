3579f74624fc0447654a438483cfde8fa3d5a100
jacobdanner <jacobdanner@granular.ag>
2022-07-20 17:25:02 -0500

    search and add new city is functional

--- weather/src/Components/Header.js
+++ weather/src/Components/Header.js
@@ -1,3 +1,28 @@
-export const Header = () => {
-    return (<h1>Weather</h1>)
+import { useState } from "react"
+
+export const Header = ({cities, setCities, setLen}) => {
+    cities = cities
+    const [input, setInput] = useState("")
+
+    const handleClick = event => {
+        console.log(cities)
+        cities.push(input)
+        setCities(cities)
+        setLen(cities.length + 1)
+        console.log(cities)
+        
+    }
+    return (
+        <>
+            <h1>Weather</h1>
+
+                <input type={"text"} 
+                    placeholder={"add city"} 
+                    onChange={event => {setInput(event.target.value)}}
+                    value={input}
+                >
+                </input>
+                <button onClick={e => handleClick(e)}>+</button>
+        </>
+    )
 }
